f7b1cedd28d1937403cccd68058065de99cb269e
alaminarchie <alaminarchie@gmail.com>
2024-10-12 16:30:20 +0600

    module 17 problem task 2

---
+++ problem-task/task2.js
@@ -0,0 +1,10 @@
+let math = 75.25;
+let biology = 65;
+let chemistry = 80;
+let physics = 35.45;
+let bangla = 99.5;
+
+let totalMarks = math + biology + chemistry + physics + bangla;
+let averageMarks = totalMarks / 5;
+
+console.log(averageMarks.toFixed(2));
