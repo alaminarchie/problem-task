8e5fe4a7cf4084976c8339c7d50bc65efddcb077
alaminarchie <alaminarchie@gmail.com>
2024-10-12 16:49:17 +0600

    folder modho theke root enechi.

---
+++ vanila/intro.js
@@ -0,0 +1,35 @@
+/**
+ * Math in JavaScript for Programming Langquage.
+ *                  *****************
+ * 1. number: positive() and Nagetive()
+ * 2. integer: Odd(85, 99, 33) and Even(44, 22, 12)
+ * 3. Float: Odd(85.45, 99.45, 33.75) and Even(44.36, 22.44, 12.21)
+ * 4. Operation: +, -, *, /
+ * 5. More Operation: ++, --, **,
+ * 6. Percentage: %, 50%, etc.
+ * 7. Sum: 15 + 12 = 27;
+ * 8. Avg: 45 + 25 + 35 = 35;
+ * 9. Max/Min: [25, 35, 65] = 65 and 25
+ * 10. Series 11. Area: Triangle
+ * 12.
+ * */
+// VARIABLE => vary + able
+// ভেরিয়েবল ডিক্লেয়ার করতে
+let age = 17;
+age = 35;
+
+let demo; //variable dicleare
+demo = "data"; // variable assign
+
+// console.log(age);
+
+// problem solving 1
+let balance = 1000;
+let costOrange = 400;
+let costApple = 300;
+
+let costOfFruits = costOrange + costApple;
+
+let moneyReturn = balance - costOfFruits;
+
+console.log(moneyReturn);
